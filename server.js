4d84931503f55e2a39f12e8e03450693f199f870
Alacritas <jhwheeler@gmail.com>
2017-02-08 09:18:01 +0530

    Add Express Router

--- server.js
+++ server.js
@@ -1,71 +1,12 @@
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const {BlogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
-
 const app = express();
 
 app.use(morgan('common'));
 
-BlogPosts.create(
-        'Hello, World!',
-        'Life is simple. Want something? Here\'s how to get it: 1. Imagine 2. Research 3. Analyze 4. Act',
-        'Alacritas');
+const postsRouter = require('./postsRouter');
 
-app.get('/blog-posts', (req, res) => {
-    res.json(BlogPosts.get());
-});
-
-app.post('/blog-posts', jsonParser, (req, res) => {
-    const requiredFields = ['title', 'content', 'author'];
-    for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`;
-            console.error(message);
-            return res.status(400).send(message);
-        }
-    }
-    const blogPost = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-    res.status(201).json(blogPost);
-});
-
-app.put('/blog-posts/:id', jsonParser, (req, res) => {
-    const requiredFields = ['title', 'content', 'author', 'publishDate'];
-    for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`;
-            console.error(message);
-            return res.status(400).send(message);
-        }
-    }
-    if (req.params.id !== req.body.id) {
-        const message = (
-            `Request path id (${req.params.id}) and request body id (${req.body.id}) must match.`);
-        console.error(message);
-        return res.status(400).send(message);
-    }
-    console.log(`Updating blog post \`${req.body.id}\``);
-    const updatedPost = BlogPosts.update({
-        id: req.params.id,
-        title: req.body.title,
-        content: req.body.content,
-        author: req.body.author,
-        publishDate: req.body.publishDate
-    });
-    res.status(204).json(updatedPost);
-});
-
-app.delete('/blog-posts/:id', (req, res) => {
-    BlogPosts.delete(req.params.id);
-    console.log(`Deleted post \`${req.params.id}\` from blog`);
-    res.status(204).end();
-});
+app.use('/blog-posts', postsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`The app is listening on port ${process.env.PORT || 8080}`);

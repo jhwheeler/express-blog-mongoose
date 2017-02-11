eec7ce0162ccd54cfc5952431b49694d039956ee
Alacritas <jhwheeler@gmail.com>
2017-02-11 18:55:57 +0530

    Add POST request

--- server.js
+++ server.js
@@ -39,6 +39,32 @@ app.get('/restaurants/:id', (req, res) => {
         });
 });
 
+app.post('/restaurants', (req, res) => {
+
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+
+    Restaurant
+        .create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        })
+        .then(
+            restaurant => res.status(201).json(restaurant.apiRep())
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({message: 'Internal Server Error'});
+        });
+});
+
 
 
 app.use('*', (req, res) => {
